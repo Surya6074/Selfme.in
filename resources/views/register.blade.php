6ff2c2b212aa2cf8fa3ec36e42c12035b6bb41f3
surya <sksurya6074@gmail.com>
2024-07-18 07:01:28 +0530

    Login design

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,8 +3,8 @@
 Login Page - Selfme
 @endsection
 @section('content')
-<div class="h-full w-full flex bg-neutral-800">
-    <div class="w-1/2 h-full flex justify-center items-center">
+<div class="h-full w-full flex md:flex-row flex-col-reverse bg-neutral-800">
+    <div class="lg:w-1/2 w-full h-full flex justify-center items-center form-page">
         <form class="flex flex-col w-2/4">
             <h1 class="text-4xl text-center font-bold mb-6 text-emerald-300">Register</h1>
             <div class="flex flex-col mb-5">
@@ -34,16 +34,18 @@ Login Page - Selfme
             </div>
         </form>
     </div>
-    <div class="w-1/2 h-full bg-emerald-300 relative info">
-
-        <div class="flex flex-col justify-center p-10 h-full">
-            <h1 class="text-neutral-100 text-4xl pl-1">Hello!</h1>
-            <h1 class="text-neutral-800 text-8xl">Make Your Own <br><span class="text-neutral-800">Portfolio<span class="text-8xl text-gray-50">.</span></span></h1>
-            <div class="mt-1 w-full pl-2">
-                <p class="mb-10 text-xl text-neutral-700">Empower Your Online Presence for Free!</p>
+    <div class="lg:w-1/2 w-full h-full bg-emerald-300 relative info form-page">
+        <div class="h-full w-full bg-neutral-950 opacity-35 absolute -z-10">s</div>
+        <div class="flex flex-col justify-center p-10 h-full z-30">
+            <h1 class="text-neutral-800 text-4xl pl-1">Hello!</h1>
+            <h1 class="text-8xl z-30 text-white">Make Your Own <br><span class="text-neutral-800 font-semibold">Portfolio<span class="text-8xl text-gray-50">.</span></span></h1>
+            <div class="mt-1 w-full pl-2 z-30">
+                <p class="mb-10 text-xl text-neutral-800">Empower Your Online Presence for Free!</p>
                 <a href="/login" class="py-3 px-8 mt-10 bg-neutral-900 text-emerald-300 rounded-lg">Login</a>
             </div>
         </div>
+        <div class="absolute bottom-8 left-1/3 ml-14 block lg:hidden z-40 text-neurtal-900">Scroll down <i class="fa-solid fa-arrow-down-long"></i></div>
+
     </div>
 </div>
 @endsection
