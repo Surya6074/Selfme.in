2aa6127e1b3d347d98d2aa8ddf50a74573086901
Suryakumar <124327594+Surya6074@users.noreply.github.com>
2024-07-25 13:58:50 +0530

    add the visibility column

--- database/migrations/2024_07_24_165117_create_portfolios_table.php
+++ database/migrations/2024_07_24_165117_create_portfolios_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('template_id');
+            $table->enum('visibility',['public','private']);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
