d4270aa47dec184779bec3e9908db6e078974238
surya <sksurya6074@gmail.com>
2024-07-27 21:33:16 +0530

    New Update tabele

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@ Portfolio Templates - Selfme
 @section('content')
 <body class="flex">
     @include('components.sidebar')
-    <section class="w-full min-h-min overflow-y-scroll p-4 mb-10">
+    <section class="w-full min-h-min overflow-y-scroll overflow-x-hidden p-4 mb-1">
         <div class="flex justify-between items-center px-5 py-3 bg-neutral-900 rounded-xl">
             <div class="flex itemes-center">
                 <button class="md:hidden block"  onclick="OpenSlidebar()"><i class="bx bx-menu-alt-left text-white text-4xl mr-5" id="sidebaricon"></i></button>
