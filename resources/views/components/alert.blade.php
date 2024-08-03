eae93958759e9fb368d77ec053b7b587b993beb6
Suryakumar <124327594+Surya6074@users.noreply.github.com>
2024-08-03 05:29:02 +0000

    Alert Components

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,10 @@
+<div class="h-screen w-screen absolute top-0 bottom-0 left-0 right-0 bg-neutral-900 flex justify-center items-center" id="alertmsg-box">
+    <div class=" bg-neutral h-full w-full absolute opacity-55 z-0">g</div>
+    <div class=" relative min-h-40 min-w-80 max-w-[300px] rounded-md z-10 flex flex-col items-center p-3 px-5 text-white bg-neutral-800">
+        <h1 class="mb-3 text-emerald-300">Alert</h1>
+        <div class="text-white text-center text-sm my-2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident sapiente nulla ratione commodi optio iste amet ut eaque, eveniet, recusandae, error impedit cum molestiae voluptatem velit at vitae dolorem sed.</p>
+        </div>
+        <button onclick="document.getElementById('alertmsg-box').remove()" class="px-5 py-2 bg-emerald-300 text-neutral-900 rounded-md">Close</button>
+    </div>
+</div>
